a4adf582a3e9d549536ce25e664ca6bcb493ed56
uotya <kawaguchi.ha@gmail.com>
2021-02-06 06:00:21 +0900

    2つのドメインからクッキーを保存させる

--- serve.js
+++ serve.js
@@ -1,25 +1,29 @@
 const express = require("express");
-const app  = express();
-const port = 8080;
 
-// POST された JSON を req.body で操作可能にする
-app.use(express.json());
-
-// ルーティングの設定
-app.get("/", (req, res) => {
-  res.json({ "text": "hello world" })
-});
-app.post('/', (req, res) => {
-  if (req.is('application/json')) {
-    res.status(201).send(req.body);
-  } else {
-    const error = new Error('Invalid Content-Type');
-    console.log(error);
-    res.status(400).send({ error: error.message });
-  }
+// app の HTTP サーバを起動する
+const app = express();
+app.use(express.static('public', {
+  setHeaders: (res, path, stat) => {
+    res.cookie('cookie', 'SampleSite', {
+      httpOnly: true,
+    });
+  },
+}));
+app.listen(8080, () => {
+  console.log('listening at http://localhost:8080');
 });
 
-// HTTP サーバを起動する
-app.listen(port, () => {
-  console.log(`listening at http://localhost:${port}`);
-});
+// image の HTTP サーバーを起動する
+const image = express()
+image.use(express.static('public-image', {
+  setHeaders: (res, path, stat) => {
+    res.cookie('cookie', 'image', {
+      httpOnly: true,
+      sameSite: 'none',
+      secure: true,
+    });
+  },
+}))
+image.listen(8081, () => {
+  console.log('listening at http://localhost:8081');
+})
